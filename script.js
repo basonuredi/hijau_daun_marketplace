8bacb86451ee698574dc032c8650518ab77367f3
basonuredi <41595283+basonuredi@users.noreply.github.com>
2026-04-30 22:04:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 JavaScript
 console.log("Marketplace siap");
+const Pi = window.Pi;
+Pi.init({ version: "2.0" });
+
+function bayarPi() {
+  Pi.createPayment({
+    amount: 0.01,
+    memo: "Pembelian Cabai",
+    metadata: { productId: "cabai_1" }
+  }, {
+    onReadyForServerApproval: function(paymentId) {
+      console.log("Ready approval:", paymentId);
+    },
+    onReadyForServerCompletion: function(paymentId, txid) {
+      console.log("Ready completion:", paymentId, txid);
+    },
+    onCancel: function(paymentId) {
+      console.log("Dibatalkan:", paymentId);
+    },
+    onError: function(error) {
+      console.error(error);
+    }
+  });
+}
