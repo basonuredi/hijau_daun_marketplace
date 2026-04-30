29ba16bd3931184f36d25d3d1ba6c539b7bfaad8
basonuredi <41595283+basonuredi@users.noreply.github.com>
2026-05-01 00:12:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-const Pi = window.Pi;
-
-Pi.init({ version: "2.0" });
-
 function bayarPi() {
-  Pi.createPayment(
-    {
-      amount: 0.01,
-      memo: "Test",
-      metadata: {}
-    },
-    {
-      onReadyForServerApproval: function(paymentId) {
-        fetch('/api/approve', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ paymentId })
-        });
-      },
-
-      onReadyForServerCompletion: function(paymentId, txid) {
-        fetch('/api/complete', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ paymentId, txid })
-        });
-      },
-
-      onCancel: function(paymentId) {
-        alert("Dibatalkan");
-      },
-
-      onError: function(error) {
-        alert("Error: " + error);
-      }
-    }
-  );
+  alert("Tombol terklik");
 }
