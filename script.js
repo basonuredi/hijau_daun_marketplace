5d5434a296fb991410b73526f93bd658a0343bbd
basonuredi <41595283+basonuredi@users.noreply.github.com>
2026-05-01 00:47:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 function bayarPi() {
   alert("Mulai");
 
-  const Pi = window.Pi;
-
-  if (!Pi) {
-    alert("❌ Pi SDK tidak terbaca");
+  if (!window.Pi) {
+    alert("❌ Pi SDK TIDAK AKTIF (domain salah)");
     return;
   }
 
-  Pi.init({ version: "2.0" });
-
-  Pi.createPayment(
-    {
-      amount: 0.01,
-      memo: "Test Payment",
-      metadata: {}
-    },
-    {
-      onReadyForServerApproval: function(paymentId) {
-        alert("✅ Masuk approval: " + paymentId);
-      },
-      onReadyForServerCompletion: function(paymentId, txid) {
-        alert("✅ Complete: " + txid);
-      },
-      onCancel: function(paymentId) {
-        alert("❌ Dibatalkan");
-      },
-      onError: function(error) {
-        alert("❌ Error: " + JSON.stringify(error));
-      }
-    }
-  );
+  alert("✅ Pi SDK AKTIF");
 }
