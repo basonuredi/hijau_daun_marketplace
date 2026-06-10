aa00c88fb4d1271932445278bd63fd03e5a791c6
basonuredi <41595283+basonuredi@users.noreply.github.com>
2026-06-10 23:49:16 +0700

    Update approve.js

--- api/approve.js
+++ api/approve.js
@@ -1,3 +1,9 @@
 export default function handler(req, res) {
-  res.status(200).json({ success: true });
+
+  console.log("APPROVE", req.body);
+
+  res.status(200).json({
+    success: true
+  });
+
 }
